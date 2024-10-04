a435e84cf56910c905d65ade453e72a802ae3373
qBlackbirdp <blackbird9711@gmail.com>
2024-10-04 16:30:51 +0900

    git test1

--- src/main/java/com/teamname/astroneer/star_info_web/controller/TestController.java
+++ src/main/java/com/teamname/astroneer/star_info_web/controller/TestController.java
@@ -12,6 +12,9 @@ public class TestController {
         // 화면에 전달할 데이터를 모델에 추가
         model.addAttribute("message", "Hello, Astroneer!");
         // Thymeleaf 뷰 파일 이름을 반환 (resources/templates/hello.html)
+
+        //깃 테스트 한영신
+        System.out.println("한영신");
         return "hello";
     }
 }
