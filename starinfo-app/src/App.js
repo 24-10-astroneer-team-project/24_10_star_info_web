2d36cf1d82848abf6aae609014c2467cd67aa03d
eunseo1030 <eunseo20001030@gmail.com>
2024-10-16 09:47:17 +0900

    로그ì메인 화면으로 넘어가지만 로그인 화면이 뜨지않ã음

--- starinfo-app/src/App.js
+++ starinfo-app/src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import LoginPage from "./components/LoginPage";
-import AstroAnimation from "./components/AstroAnimation";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import MainPage from './components/MainPage';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
     return (
-        <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-            {/* PixiJS 애니메이션 */}
-            <AstroAnimation />
-            {/* 로그인 페이지 */}
-            <div style={{ position: "absolute", top: 0, left: 0, width: "100%", zIndex: 1 }}>
-                <LoginPage />
-            </div>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/react/main" element={<MainPage />} />   {/* 메인 페이지로 라우팅 */}
+                <Route path="/react/login" element={<LoginPage />} />  {/* 로그인 페이지로 라우팅 */}
+                <Route path="*" element={<NotFoundPage />} />  {/* 404 처리 */}
+            </Routes>
+        </Router>
     );
 }
 
--- starinfo-app/src/components/LoginPage.js
+++ starinfo-app/src/components/LoginPage.js
@@ -1,17 +1,23 @@
 import React from "react";
 import GoogleLoginButton from "./GoogleLoginButton";
 import LogoutButton from "./LogoutButton";
-import './LoginPage.css';  // CSS 파일 추가
+import AstroAnimation from "../pixi/AstroAnimation"; // 애니메이션 컴포넌트 추가
+import './LoginPage.css'; // CSS 파일 추가
 
 function LoginPage() {
     return (
-        <div className="login-container">
-            <h2 className="login-title">로그인 방법을 선택해주세요</h2>
-            {/* Google 로그인 버튼 */}
-            <GoogleLoginButton />
+        <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+            {/* Astro 애니메이션 */}
+            <AstroAnimation />
 
-            {/* 로그아웃 버튼 */}
-            <LogoutButton />
+            {/* 로그인 화면 요소 */}
+            <div className="login-container" style={{ position: "absolute", top: 0, left: 0, width: "100%", zIndex: 1 }}>
+                <h2 className="login-title">로그인 방법을 선택해주세요</h2>
+
+                {/* Google 로그인 버튼 */}
+                <GoogleLoginButton />
+
+            </div>
         </div>
     );
 }
