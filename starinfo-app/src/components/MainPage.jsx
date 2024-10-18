d6c8b6e390b0612a61cf0e2dbc14c2c5b8ab4ef0
eunseo1030 <eunseo20001030@gmail.com>
2024-10-18 12:41:02 +0900

    ì섹션 나누는 중

--- starinfo-app/src/components/MainPage.jsx
+++ starinfo-app/src/components/MainPage.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useRef } from 'react';
-import styles from '../main/SecondSection.module.css';  // CSS 모듈 불러오기
+import stylesFirst from '../main/firstSection.module.css'; // 첫 번째 섹션 CSS
+
 
 function MainPage() {
-    const textRef = useRef(null);
-
-    useEffect(() => {
-        // 텍스트 애니메이션 시작 또는 기타 이펙트 추가 가능
-    }, []);
-
     return (
-        <div style={{ width: '100%', height: '100vh' }}>
-            {/* 첫 번째 세션 */}
-            <div className={styles.secondSection}>
-                <div className={styles.secondSectionText}>
+        <div style={{ width: '100%', height: '200vh' }}> {/* 전체 200vh로 구성 */}
+
+            {/* 첫 번째 섹션 */}
+            <div className={stylesFirst.firstSection}>
+                <div className={stylesFirst.firstSectionText}>
                     Hello earthling
                 </div>
             </div>
+
+
+            {/* 두 번째 섹션 */}
+            <div>
+                <div >
+                    Hello Universe
+                </div>
+            </div>
+
         </div>
     );
 }
