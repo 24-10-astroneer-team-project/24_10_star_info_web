5bca920376e86739328617931cf86f7c1685a4aa
hrjo0120 <hrjo0120@gmail.com>
2024-10-21 09:39:50 +0900

    행성 컴포넌트 및 태양계 이미지 추가
    - 개별 행성 렌더링을 위한 Planet.jsx 및 PlanetPage.jsx 추가
    - 태양계 레이아웃 SolarSystem.jsx 통합
    - 행성 이미지 추가
    - Planet.css 추가

--- starinfo-app/src/components/GoogleLoginButton.jsx
+++ starinfo-app/src/components/GoogleLoginButton.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import "./GoogleLoginButton.css";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 const GoogleLoginButton = () => {
-  const googleLogin = () => {
-    console.log("%c[INFO] Google 로그인 시도 중...", "color: blue");
-    const backendUrl =
-      process.env.REACT_APP_BACKEND_URL || "http://localhost:7777";
-    window.location.href = `${backendUrl}/oauth2/authorization/google`; // Google OAuth2 로그인 경로
-  };
+    const googleLogin = () => {
+        console.log("%c[INFO] Google 로그인 시도 중...", "color: blue");
+        const backendUrl =
+            process.env.REACT_APP_BACKEND_URL || "http://localhost:7777";
+        window.location.href = `${backendUrl}/oauth2/authorization/google`; // Google OAuth2 로그인 경로
+    };
 
-  return (
-    <motion.button // 임시
-      onClick={googleLogin}
-      className="google-login-button"
-      whileHover={{ scale: 1.1 }} // 호버 시 확대
-      whileTap={{ scale: 0.9 }} // 클릭 시 축소
-    >
-      <div className="bc-logo">
-        <img
-          src="https://developers.google.com/identity/images/g-logo.png"
-          className="google-logo"
-          alt="Google logo"
-        />
-      </div>
-      <span className="login-text">Sign in with Google</span>
-    </motion.button>
-  );
+    return (
+        <motion.button // 임시
+            onClick={googleLogin}
+            className="google-login-button"
+            whileHover={{scale: 1.1}} // 호버 시 확대
+            whileTap={{scale: 0.9}} // 클릭 시 축소
+        >
+            <div className="bc-logo">
+                <img
+                    src="https://developers.google.com/identity/images/g-logo.png"
+                    className="google-logo"
+                    alt="Google logo"
+                />
+            </div>
+            <span className="login-text">Sign in with Google</span>
+        </motion.button>
+    );
 };
 
 export default GoogleLoginButton;
---
+++ starinfo-app/src/components/SolarSystem.jsx
@@ -0,0 +1,104 @@
+import React, {useState} from "react";
+import './Planet.css';
+
+const SolarSystem = ({solarSystemClass, handlePlanetClick}) => {
+    return (
+        <div>
+            <div id="solar-system" className={solarSystemClass}>
+                <div id="mercury" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="venus" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Venus</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="earth" className="orbit">
+                    <div className="pos">
+                        <div className="orbit">
+                            <div className="pos">
+                                <div className="moon"></div>
+                            </div>
+                        </div>
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Earth</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="mars" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Mars</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="jupiter" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Jupiter</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="saturn" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <div className="ring"></div>
+                            <dl className="infos">
+                                <dt>Saturn</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="uranus" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Uranus</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="neptune" className="orbit">
+                    <div className="pos">
+                        <div className="planet">
+                            <dl className="infos">
+                                <dt>Neptune</dt>
+                                <dd><span></span></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div id="sun">
+                    <dl className="infos">
+                        <dt>Sun</dt>
+                        <dd><span></span></dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SolarSystem;
