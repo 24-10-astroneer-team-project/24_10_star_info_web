c62871cf50bc4c2c7237eebac83bdd54d46e4b27
eunseo1030 <eunseo20001030@gmail.com>
2024-10-23 15:41:07 +0900

    2번째 섹션 구현 완료

--- starinfo-app/src/components/MainPage.jsx
+++ starinfo-app/src/components/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import stylesFirst from '../main1/firstSection.module.css';
 import EarthAnimation from '../main2/EarthAnimation';
+import stylesSecond from '../main2/EarthSection.module.css'; // 올바른 파일명으로 수정
 
 function MainPage() {
     return (
-        <div style={{ width: '100%', height: '200vh' }}>
+        <div style={{ width: '100%', height: '300vh' }}>
             {/* 첫 번째 섹션 */}
             <div className={stylesFirst.firstSection}>
                 <div className={stylesFirst.firstSectionText}>
@@ -13,9 +14,12 @@ function MainPage() {
             </div>
 
             {/* 두 번째 섹션: Earth Animation */}
-            <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+            <div className={stylesSecond.earthSection}> {/* 수정된 CSS 클래스 적용 */}
                 <EarthAnimation />
             </div>
+
+            {/* 세 번째 섹션: Solar System */}
+
         </div>
     );
 }
