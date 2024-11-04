f4b309d1d77bc8294a18e0ef5dd2c8e67b09b67c
qBlackbirdp <blackbird9711@gmail.com>
2024-11-04 09:25:39 +0900

    Remove node_modules folder

--- starinfo-app/src/services/googleMapsService.js
+++ starinfo-app/src/services/googleMapsService.js
@@ -3,7 +3,11 @@
 export const loadPlaceDetails = (autocomplete, setLocation) => {
     if (autocomplete) {
         const place = autocomplete.getPlace();
-        const { lat, lng } = place.geometry.location;
-        setLocation({ lat: lat(), lng: lng() });
+        if (place && place.geometry && place.geometry.location) { // geometry 속성 확인
+            const { lat, lng } = place.geometry.location;
+            setLocation({ lat: lat(), lng: lng() });
+        } else {
+            console.error("No place details available.");
+        }
     }
 };
