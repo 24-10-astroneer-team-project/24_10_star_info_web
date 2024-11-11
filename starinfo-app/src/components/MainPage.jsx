d9897f09d9a834532e9ed0e6f3d29149981aa213
eunseo1030 <eunseo20001030@gmail.com>
2024-11-11 09:22:02 +0900

    head와 foot 수정 완료 메인 페이지 수정 중

--- starinfo-app/src/components/MainPage.jsx
+++ starinfo-app/src/components/MainPage.jsx
@@ -1,4 +1,3 @@
-// src/MainPage.js
 import React, { useEffect, useRef, useState } from 'react';
 import './MainPage.css';
 import stylesFirst from '../main/main1/firstSection.module.css';
@@ -7,8 +6,8 @@ import stylesThird from '../main/main3/ThirdSection.css';
 import SolarSystem from '../main/main3/SolarSystem.jsx';
 import WaveCanvas from '../main/main4/WaveCanvas';
 import ConstellationSection from '../main/main5/ConstellationSection';
-import Head from "./layout/Head";
-import Foot from "./layout/Foot";
+import Head from './layout/Head';
+import Foot from './layout/Foot';
 
 function MainPage() {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -55,11 +54,13 @@ function MainPage() {
 
     return (
         <>
-
+            {/* 모든 섹션에서 헤더가 보이도록 고정 */}
+            <Head />
             <div style={{ overflow: 'hidden', height: '100vh', position: 'relative' }}>
                 <div ref={containerRef} style={{ width: '100%', transition: 'transform 0.7s ease' }}>
+                    {/* 각 섹션 */}
                     <div ref={(el) => (sections.current[0] = el)} className={stylesFirst.firstSection} style={{ height: '100vh' }}>
-                        <div className={stylesFirst.firstSectionText}>Hello Earthling</div>
+                        <div className={stylesFirst.firstSectionText}>Hello earthling</div>
                     </div>
                     <div ref={(el) => (sections.current[1] = el)} style={{ height: '100vh' }}>
                         <WeatherPage />
@@ -70,11 +71,16 @@ function MainPage() {
                     <div ref={(el) => (sections.current[3] = el)} style={{ height: '100vh', backgroundColor: 'black' }}>
                         <WaveCanvas />
                     </div>
-                    <div ref={(el) => (sections.current[4] = el)} style={{ height: '100vh' }}>
+                    <div ref={(el) => (sections.current[4] = el)} style={{ height: '100vh', position: 'relative' }}>
                         <ConstellationSection />
+                        {/* 푸터는 마지막 섹션 하단에 고정 */}
+                        <div style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+                            <Foot />
+                        </div>
                     </div>
                 </div>
 
+                {/* Dot Navigation */}
                 <div className="dot-navigation">
                     {sections.current.map((_, index) => (
                         <div
@@ -85,7 +91,6 @@ function MainPage() {
                     ))}
                 </div>
             </div>
-
         </>
     );
 }
