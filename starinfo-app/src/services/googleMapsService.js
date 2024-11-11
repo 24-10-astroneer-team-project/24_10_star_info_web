7db6d65b3075a403eaad864d0dfab62057b587c7
qBlackbirdp <blackbird9711@gmail.com>
2024-11-11 19:13:41 +0900

    develop edit all, pull 하세요

--- starinfo-app/src/services/googleMapsService.js
+++ starinfo-app/src/services/googleMapsService.js
@@ -1,6 +1,5 @@
 //googleMapsService.js
 
-
 export const loadPlaceDetails = (autocomplete, setLocation) => {
     if (autocomplete) {
         const place = autocomplete.getPlace();
@@ -12,16 +11,3 @@ export const loadPlaceDetails = (autocomplete, setLocation) => {
         }
     }
 };
-
-// 장소 상세 정보를 로드하는 함수
-export const loadPlaceDetails = (autocomplete, setLocation) => {
-    const place = autocomplete.getPlace();
-    if (place && place.geometry && place.geometry.location) {
-        const lat = place.geometry.location.lat();
-        const lng = place.geometry.location.lng();
-        setLocation({ lat, lng }); // 상태 업데이트
-    } else {
-        console.error("No place details available.");
-    }
-};
-
