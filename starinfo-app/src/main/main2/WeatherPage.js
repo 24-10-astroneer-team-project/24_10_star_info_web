02b17dcb8a2124d83f5d1f82d165ffdf462f0c83
Pive0528 <pive0528@gmail.com>
2024-11-11 09:26:45 +0900

    merge 전 갱신

--- starinfo-app/src/main/main2/WeatherPage.js
+++ starinfo-app/src/main/main2/WeatherPage.js
@@ -1,12 +1,13 @@
 // src/main2/WeatherPage.js
 import React from 'react';
 import styles from './WeatherPage.module.css';
-
+import Main_Button from '../../components/layout/Main_Button'; // 임시추가'
 function WeatherPage() {
     return (
         <div className={styles.bg}>
             {/* 상단의 'Local Weather App' 텍스트만 표시 */}
             <h1 className={styles.textCenter}>Local Weather App</h1>
+            <Main_Button/>
         </div>
     );
 }
