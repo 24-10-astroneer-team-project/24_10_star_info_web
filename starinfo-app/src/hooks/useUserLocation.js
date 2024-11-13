a50c9c548b3f0fc85447aeecaabafd464a42dbd0
qBlackbirdp <blackbird9711@gmail.com>
2024-11-13 09:47:36 +0900

    로그인시 중복 API 요청 해결

--- starinfo-app/src/hooks/useUserLocation.js
+++ starinfo-app/src/hooks/useUserLocation.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useAuth } from "../services/AuthProvider";
-import {getUserLocationFromProfile} from "../services/userLocationService";
+import { getUserLocationFromProfile } from "../services/userLocationService";
 
 function useUserLocation() {
     const [location, setLocation] = useState({ latitude: 37.5665, longitude: 126.9780 }); // 기본값: 서울
@@ -10,19 +10,34 @@ function useUserLocation() {
     const { isAuthenticated } = useAuth();  // 로그인 여부를 확인
 
     useEffect(() => {
+        let isMounted = true; // 컴포넌트가 언마운트될 때 실행 방지
+
         // 로그인 여부 확인 후 위치 설정
-        if (isAuthenticated) {
-            // 사용자가 설정한 위치 가져오기
-            const userLocation = getUserLocationFromProfile(); // 사용자 테이블에서 위치를 가져오는 함수
-            if (userLocation) {
-                setLocation(userLocation);
+        const fetchUserLocation = async () => {
+            if (isAuthenticated) {
+                const userLocation = getUserLocationFromProfile(); // 사용자 테이블에서 위치를 가져오는 함수
+                if (userLocation && isMounted) {
+                    setLocation(userLocation);
+                }
+            } else {
+                // 로그인하지 않았을 때는 서울로 위치 고정
+                setLocation({ latitude: 37.5665, longitude: 126.9780 });
             }
-        } else {
-            // 로그인하지 않았을 때는 서울로 위치 고정
-            setLocation({ latitude: 37.5665, longitude: 126.9780 });
+            setIsLoading(false);
+        };
+
+        if (isLoading) {
+            fetchUserLocation().then(() => {
+                console.log('사용자 저장 위치로 요청 성공');
+            }).catch((error) => {
+                console.error('Error fetching user location:', error);
+            });
         }
-        setIsLoading(false);
-    }, [isAuthenticated]); // isAuthenticated가 변경될 때마다 실행
+
+        return () => {
+            isMounted = false; // 컴포넌트가 언마운트되었을 때 업데이트 방지
+        };
+    }, [isAuthenticated, isLoading]);
 
     return { location, isLoading };
 }
