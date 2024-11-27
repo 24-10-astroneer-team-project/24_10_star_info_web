7b9450757ef54fc65bbb2d8e9e19ac0673939f28
Pive0528 <pive0528@gmail.com>
2024-11-27 09:43:31 +0900

    외부 클릭으로 팝업창이 닫히도록 수정

--- starinfo-app/src/components/StarMap/FavoriteLocationPopup.jsx
+++ starinfo-app/src/components/StarMap/FavoriteLocationPopup.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import './css/FavoriteLocationPopup.css';
 
 function FavoriteLocationPopup({ locationDescription, constellations, onClose }) {
+    // 팝업 내부 클릭 이벤트 핸들러
+    const handlePopupClick = (event) => {
+        event.stopPropagation(); // 내부 클릭 시 외부 클릭 이벤트 전파 차단
+    };
+
+    // 팝업 외부 클릭 이벤트 핸들러
+    const handleOverlayClick = () => {
+        onClose(); // 팝업 닫기
+    };
+
     return (
-        <div className="favorite-location-popup">
-            <div className="popup-content">
+        <div className="favorite-location-popup" onClick={handleOverlayClick}>
+            <div className="popup-content" onClick={handlePopupClick}>
                 <h2>즐겨찾기한 위치 정보</h2>
                 <p>설명: {locationDescription}</p>
                 <p>해당 위치에서 가장 잘 보이는 별자리 데이터:</p>
@@ -22,9 +32,6 @@ function FavoriteLocationPopup({ locationDescription, constellations, onClose })
                 ) : (
                     <p>별자리 데이터가 없습니다.</p>
                 )}
-                <button className="close-button" onClick={onClose}>
-                    닫기
-                </button>
             </div>
         </div>
     );
