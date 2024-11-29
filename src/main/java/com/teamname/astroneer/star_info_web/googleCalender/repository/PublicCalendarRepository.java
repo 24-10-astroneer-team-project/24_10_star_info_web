075391cda52b4c947ba183994af0251113127173
qBlackbirdp <blackbird9711@gmail.com>
2024-11-29 09:32:42 +0900

    add

--- src/main/java/com/teamname/astroneer/star_info_web/googleCalender/repository/PublicCalendarRepository.java
+++ src/main/java/com/teamname/astroneer/star_info_web/googleCalender/repository/PublicCalendarRepository.java
@@ -5,10 +5,14 @@ import com.teamname.astroneer.star_info_web.googleCalender.entity.PublicCalendar
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PublicCalendarRepository extends JpaRepository<PublicCalendar, Long> {
 
     Optional<PublicCalendar> findByGoogleEventId(String googleEventId);
+
+    List<PublicCalendar> findByStartDateTimeBetween(LocalDateTime localDateTime, LocalDateTime localDateTime1);
 }
