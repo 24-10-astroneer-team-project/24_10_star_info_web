f3f2fc39539869bfd9d54f9e6b627c1a91bf57c3
qBlackbirdp <blackbird9711@gmail.com>
2024-12-06 16:42:40 +0900

    자동배포 테스트

--- src/main/java/com/teamname/astroneer/star_info_web/StarInfoWebApplication.java
+++ src/main/java/com/teamname/astroneer/star_info_web/StarInfoWebApplication.java
@@ -12,18 +12,18 @@ public class StarInfoWebApplication {
         Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
 
         // .env 파일에서 환경 변수 설정
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
-        System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+//        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+//        System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
+//        System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
         System.out.println(Dotenv.configure().ignoreIfMissing().load());
-        System.out.println(System.getProperty("JWT_SECRET"));
+//        System.out.println(System.getProperty("JWT_SECRET"));
 
         // Spring Boot 애플리케이션 실행
         SpringApplication.run(StarInfoWebApplication.class, args);
 
-        System.out.println(System.getProperty("JWT_SECRET"));
+//        System.out.println(System.getProperty("JWT_SECRET"));
     }
 }
